a08fccca75ce6efd410cfbcf6027d00eed5c4bc4
Juan <jroussilian@fi.uba.ar>
2023-10-01 02:43:16 -0300

    agrego alias a modulo de constantes para que el linter no se queje

--- server/ServerStopAndWait.py
+++ server/ServerStopAndWait.py
@@ -1,32 +1,32 @@
 import hashlib
 import os
-import math
 import time
 from common.Logger import *
 from common.Checksum import *
 from common.Packet import Packet
-from common.constants import *
+import common.constants as const
 
 class ServerStopAndWait:
+
     def __init__(self, socket, clientAddress, clientPort, storage):
         self.socket = socket
         self.clientAddress = clientAddress
         self.clientPort = clientPort
-        self.protocolID = bytes([STOP_AND_WAIT])
+        self.protocolID = bytes([const.STOP_AND_WAIT])
         self.storage = storage
 
     def send(self, message):
         self.socket.send(message, self.clientAddress, self.clientPort)
 
     def sendFileTransferTypeResponse(self, fileSize):
-        opcode = bytes([FILE_TRANSFER_RESPONSE_OPCODE])
-        zeroedChecksum = (0).to_bytes(4, BYTEORDER)
-        nseq = (0).to_bytes(4, BYTEORDER)
+        opcode = bytes([const.FILE_TRANSFER_RESPONSE_OPCODE])
+        zeroedChecksum = (0).to_bytes(4, const.BYTEORDER)
+        nseq = (0).to_bytes(4, const.BYTEORDER)
         finalChecksum = Checksum.get_checksum(zeroedChecksum + opcode  + nseq, len(opcode + zeroedChecksum + nseq), 'sendACK')
         header = (opcode, finalChecksum, nseq)
 
         # fixed chunksize (4096 bytes)
-        chunksize = CHUNKSIZE.to_bytes(4, BYTEORDER)
+        chunksize = const.CHUNKSIZE.to_bytes(4, const.BYTEORDER)
 
         message = Packet.pack_file_transfer_type_response(header, chunksize)
         self.send(message)
@@ -34,7 +34,7 @@ class ServerStopAndWait:
     def upload(self, fileSize, fileName, originalMd5):
         fileName = fileName.rstrip('\x00')
         self.sendFileTransferTypeResponse(fileSize)
-        totalPackets = fileSize / CHUNKSIZE
+        totalPackets = fileSize / const.CHUNKSIZE
         acksSent = 0
         nextNseq = 1
         file = []
@@ -53,7 +53,7 @@ class ServerStopAndWait:
             else: # client resends packet - cases 3 (lost ACK) and 4 (timeout)
                 self.sendACK(header['nseq']) # server only resends ACK (detects duplicate)
 
-        bytesInLatestPacket = fileSize % CHUNKSIZE
+        bytesInLatestPacket = fileSize % const.CHUNKSIZE
         Logger.LogWarning(f"There are {bytesInLatestPacket} bytes on the las packet. removing padding")
         file[len(file)-1] = file[len(file)-1][0:bytesInLatestPacket]
         Logger.LogWarning(f"Padding removed")
@@ -68,14 +68,14 @@ class ServerStopAndWait:
         pass
 
     def receivePackage(self):
-        received_message, (serverAddres, serverPort) = self.socket.receive(PACKET_SIZE)
+        received_message, (serverAddres, serverPort) = self.socket.receive(const.PACKET_SIZE)
         header, payload = Packet.unpack_package(received_message)
         return header, payload
     
     def sendACK(self, nseq):
-        opcode = bytes([ACK_OPCODE])
-        checksum = (2).to_bytes(4, BYTEORDER)
-        nseqToBytes = nseq.to_bytes(4, BYTEORDER)
+        opcode = bytes([const.ACK_OPCODE])
+        checksum = (2).to_bytes(4, const.BYTEORDER)
+        nseqToBytes = nseq.to_bytes(4, const.BYTEORDER)
         header = (opcode, checksum, nseqToBytes)
 
         message = Packet.pack_ack(header)
@@ -93,16 +93,16 @@ class ServerStopAndWait:
         fileWriter.close()
 
     def isChecksumOK(self, header, payload):
-        opcode = header['opcode'].to_bytes(1, BYTEORDER)
-        checksum = (header['checksum']).to_bytes(4, BYTEORDER)
-        nseqToBytes = header['nseq'].to_bytes(4, BYTEORDER)
+        opcode = header['opcode'].to_bytes(1, const.BYTEORDER)
+        checksum = (header['checksum']).to_bytes(4, const.BYTEORDER)
+        nseqToBytes = header['nseq'].to_bytes(4, const.BYTEORDER)
         
         return Checksum.is_checksum_valid(checksum + opcode + nseqToBytes + payload, len(opcode + checksum + nseqToBytes + payload))
 
     def stopFileTransfer(self, nseq, fileName, md5, state):
-        opcode = bytes([STOP_FILE_TRANSFER_OPCODE])
-        zeroedChecksum = (0).to_bytes(4, BYTEORDER)
-        nseqToBytes = nseq.to_bytes(4, BYTEORDER)
+        opcode = bytes([const.STOP_FILE_TRANSFER_OPCODE])
+        zeroedChecksum = (0).to_bytes(4, const.BYTEORDER)
+        nseqToBytes = nseq.to_bytes(4, const.BYTEORDER)
         finalChecksum = Checksum.get_checksum(zeroedChecksum + opcode  + nseqToBytes, len(opcode + zeroedChecksum + nseqToBytes), 'sendACK')
         header = (opcode, finalChecksum, nseqToBytes)
         
@@ -120,17 +120,17 @@ class ServerStopAndWait:
         communicationFinished = False
         stopCommunicationSocketTimeout = 0
 
-        while (not communicationFinished) and (stopCommunicationSocketTimeout < LAST_ACK_PACKET_TIMEOUT):
+        while (not communicationFinished) and (stopCommunicationSocketTimeout < const.LAST_ACK_PACKET_TIMEOUT):
            try:
                self.socket.settimeout(0.2)
-               received_message, (serverAddres, serverPort) = self.socket.receive(ACK_SIZE)
+               received_message, (serverAddres, serverPort) = self.socket.receive(const.ACK_SIZE)
                stopCommunicationSocketTimeout = 0
                communicationFinished = True
            except TimeoutError:
                # Acá se da por sentado que el cliente se cerró
                stopCommunicationSocketTimeout += 1
 
-           if (not communicationFinished) and (time.time() - stopFileTransferMsgSentAt > SELECTIVE_REPEAT_PACKET_TIMEOUT):
+           if (not communicationFinished) and (time.time() - stopFileTransferMsgSentAt > const.SELECTIVE_REPEAT_PACKET_TIMEOUT):
                 self.send(message)
                 stopFileTransferMsgSentAt = time.time()            
 
@@ -144,9 +144,9 @@ class ServerStopAndWait:
         Logger.LogDebug(f"File server MD5: \t{md5.hexdigest()}")
         Logger.LogDebug(f"Client's MD5: \t\t{originalMd5.hex()}")        
         
-        state = bytes([STATE_ERROR]) # Not okay by default
+        state = bytes([const.STATE_ERROR]) # Not okay by default
         if md5.hexdigest() == originalMd5.hex():
-            state = bytes([STATE_OK])
+            state = bytes([const.STATE_OK])
         
         return md5, state
 
