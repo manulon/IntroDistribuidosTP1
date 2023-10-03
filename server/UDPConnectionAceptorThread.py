5062dbe0388cd45b7449331e799f6762be98a130
manulon <mlongoe@fi.uba.ar>
2023-10-02 21:21:25 -0300

    linter for the connection aceptor thread

--- server/UDPConnectionAceptorThread.py
+++ server/UDPConnectionAceptorThread.py
@@ -1,10 +1,14 @@
 import threading
-from common.constants import *
-from common.Socket import *
+from common.Logger import Logger
+from common.Utils import Utils
+from common.Checksum import Checksum
+from common.Packet import Packet
+from common.constants import COLOR_BLUE, COLOR_END, \
+    FILE_TRANSFER_REQUEST_SIZE, BYTEORDER
+from common.Socket import Socket
 from server.ServerStopAndWait import ServerStopAndWait
-from server.UDPServerThread import *
-from server.ServerSelectiveRepeat import *
-from server.ServerStopAndWait import ServerStopAndWait
+from server.UDPServerThread import UDPServerThread
+from server.ServerSelectiveRepeat import ServerSelectiveRepeat
 
 
 class UDPConnectionAceptorThread(threading.Thread):
@@ -24,7 +28,9 @@ class UDPConnectionAceptorThread(threading.Thread):
             if not firstPacketIsValid:
                 try:
                     self.serverSocket.settimeout(0.2)
-                    firstPacketIsValid, header, payload, clientAddress, clientPort = self.receiveFirstPacket()
+                    firstPacketIsValid, header, payload, \
+                        clientAddress, clientPort = \
+                        self.receiveFirstPacket()
                     if firstPacketIsValid:
                         if (clientAddress, clientPort) not in self.clients:
                             print(
@@ -32,40 +38,56 @@ class UDPConnectionAceptorThread(threading.Thread):
                                 f"{COLOR_END}"
                                 f" - New UDP Client {clientAddress}"
                                 f":{clientPort} connected.")
-                        
+
                         self.lastSocketPort += 1
-                        newSocket = Socket(self.lastSocketPort, self.serverAddress)
-                        
+                        newSocket = Socket(
+                            self.lastSocketPort, self.serverAddress)
+
                         match payload['protocol']:
-                            case 2: # STOP & WAIT
-                                protocol = ServerStopAndWait(newSocket, clientAddress, clientPort, self.storage)
+                            case 2:  # STOP & WAIT
+                                protocol = ServerStopAndWait(newSocket,
+                                                             clientAddress,
+                                                             clientPort,
+                                                             self.storage)
                                 Logger.LogInfo('Selected stop and wait')
 
-                            case 1: # SELECTIVE REPEAT
-                                protocol = ServerSelectiveRepeat(newSocket, clientAddress, clientPort, self.storage)
+                            case 1:  # SELECTIVE REPEAT
+                                protocol = ServerSelectiveRepeat(newSocket,
+                                                                 clientAddress,
+                                                                 clientPort,
+                                                                 self.storage)
                                 Logger.LogInfo('Selected Selective Repeat')
 
-                        self.clients[(clientAddress, clientPort)] = UDPServerThread(protocol, header, payload)
+                        self.clients[(clientAddress,
+                                      clientPort)] = \
+                            UDPServerThread(protocol,
+                                            header,
+                                            payload)
                         self.clients[(clientAddress, clientPort)].start()
-                except:
+                except BaseException:
                     continue
 
     def receiveFirstPacket(self):
-        received_message, (clientAddress, clientPort) = self.serverSocket.receive(FILE_TRANSFER_REQUEST_SIZE)
-        
+        received_message, (clientAddress, clientPort) \
+            = self.serverSocket.receive(
+                FILE_TRANSFER_REQUEST_SIZE)
+
         header = None
         payload = None
 
-        Logger.LogDebug(f"I receive a packet with opcode {Utils.bytesToInt(received_message[:1])}")
+        Logger.LogDebug(
+            f"I receive a packet with opcode \
+                {Utils.bytesToInt(received_message[:1])}")
 
-        if Utils.bytesToInt(received_message[:1]) == 0:    
+        if Utils.bytesToInt(received_message[:1]) == 0:
             header, payload = Packet.unpack_upload_request(received_message)
         elif Utils.bytesToInt(received_message[:1]) == 2:
             header, payload = Packet.unpack_download_request(received_message)
-        
+
         firstPacketIsValid = self.isChecksumOK(header, payload)
 
-        Logger.LogDebug(f"Is the first packet valid? The answer is {firstPacketIsValid}")
+        Logger.LogDebug(
+            f"Is the first packet valid? The answer is {firstPacketIsValid}")
 
         return firstPacketIsValid, header, payload, clientAddress, clientPort
 
@@ -74,8 +96,13 @@ class UDPConnectionAceptorThread(threading.Thread):
         opcode = header['opcode'].to_bytes(1, BYTEORDER)
         checksum = (header['checksum']).to_bytes(4, BYTEORDER)
         nseqToBytes = header['nseq'].to_bytes(1, BYTEORDER)
-        
-        return Checksum.is_checksum_valid(checksum + opcode  + nseqToBytes, len(opcode + checksum + nseqToBytes))
+
+        return Checksum.is_checksum_valid(checksum +
+                                          opcode +
+                                          nseqToBytes,
+                                          len(opcode +
+                                              checksum +
+                                              nseqToBytes))
 
     def force_stop(self):
         self.allowedToRun = False
