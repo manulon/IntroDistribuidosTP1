f7edd1796faed0b5011331526137d427840aa44c
manulon <mlongoe@fi.uba.ar>
2023-10-02 21:26:00 -0300

    linter is ok

--- upload.py
+++ upload.py
@@ -3,9 +3,11 @@ import getopt
 import os
 from client.Client import Client
 from client.ClientSelectiveRepeat import ClientSelectiveRepeat
-from common.constants import *
-from client.ClientStopAndWait import *
+from common.constants import LOG_LEVEL_WARNING, \
+    LOG_LEVEL_DEBUG, LOG_LEVEL_ERROR
+from client.ClientStopAndWait import ClientStopAndWait
 from common.Logger import Logger
+
 
 def main(argv):
     Logger.SetLogLevel(LOG_LEVEL_WARNING)
@@ -13,18 +15,24 @@ def main(argv):
     port_service_port = 15000  # por defecto 15000
     storage = ""
     file_name = None
-    
+
     try:
-        opts, args = getopt.getopt(argv, "hvqH:p:d:n:", ["help", "verbose", "quiet", "host=", "port=", "dst=", "name="])
+        opts, args = getopt.getopt(
+            argv, "hvqH:p:d:n:", [
+                "help", "verbose", "quiet", "host=", "port=", "dst=", "name="])
     except getopt.GetoptError:
         sys.exit(2)
 
     for opt, arg in opts:
         # HELP
-        if opt in ("-h", "--help"): 
-            print("usage: upload [-h] [-v | -q] [-H ADDR] [-p PORT] [-s FILEPATH] [-n FILENAME]")
+        if opt in ("-h", "--help"):
+            print(
+                "usage: upload [-h] [-v | -q] [-H ADDR] \
+                    [-p PORT] [-s FILEPATH] [-n FILENAME]")
             print()
-            print("Upload a file up to 4 gb to a server, using UDP over an RDT connection")
+            print(
+                "Upload a file up to 4 gb to a server, \
+                    using UDP over an RDT connection")
             print("It supports:")
             print("\tSelective Repeat")
             print("\tStop and wait")
@@ -38,7 +46,7 @@ def main(argv):
             print("  -s, --src      source file path")
             print("  -n, --name     file name")
             return
-        
+
         # VERBOSE
         elif opt in ("-v", "--verbose"):
             Logger.SetLogLevel(LOG_LEVEL_DEBUG)
@@ -47,18 +55,18 @@ def main(argv):
         # QUIET
         elif opt in ("-q", "--quiet"):
             Logger.LogInfo("Verbosity will now be set to Error")
-            Logger.SetLogLevel(LOG_LEVEL_ERROR)            
-            
+            Logger.SetLogLevel(LOG_LEVEL_ERROR)
+
         # HOST
         elif opt in ("-H", "--host"):
             host_service_ip_address = arg
             Logger.LogInfo(f"Host set to {host_service_ip_address}")
-            
+
         # PORT
         elif opt in ("-p", "--port"):
             port_service_port = int(arg)
             Logger.LogInfo(f"Port set to {port_service_port}")
-            
+
         # SOURCE
         elif opt in ("-s", "--src"):
             storage = arg
@@ -75,18 +83,21 @@ def main(argv):
             if file_name:
                 Logger.LogInfo(f"File name {file_name}")
             if len(file_name) > 20:
-                Logger.LogError(f"File name too long. It should be 20 characters or less")
+                Logger.LogError(
+                    "File name too long. It should be 20 characters or less")
                 return
 
-    if file_name == None or file_name == "" or not file_name:
+    if file_name is None or file_name == "" or not file_name:
         Logger.LogError("No file specified")
         return
 
     client = Client(host_service_ip_address, port_service_port, storage)
 
     has_protocol = False
-    while (has_protocol == False):
-        protocol = input('What protocol do you want to use?: \n 1) Selective Repeat \n 2) Stop and Wait \n')
+    while not has_protocol:
+        protocol = input(
+            'What protocol do you want to use?: \n \
+                1) Selective Repeat \n 2) Stop and Wait \n')
         if (protocol == '1'):
             client.setProtocol(ClientSelectiveRepeat())
             has_protocol = True
@@ -99,6 +110,7 @@ def main(argv):
 
     client.upload(file_name)
     client.close()
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
